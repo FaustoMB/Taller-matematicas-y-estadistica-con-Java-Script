dc7a4441ceec18c6fc6d7229f15062346bc67c77
Fausto Mastrodomenico <faustomastro96@gmail.com>
2024-01-22 17:24:03 -0500

    Se agrega a la función de calculo de descuentos las diferentes condiciones para funcionar, además de la funcionalidad del ingreso de un cupón a partir de un objeto

--- statistics.js
+++ statistics.js
@@ -2,11 +2,36 @@ const precio = document.querySelector('#precio')
 const descuento = document.querySelector('#descuento')
 const resultado = document.querySelector('#precioConDescuento')
 const calcular = document.querySelector('#calcular')
+const cupon = document.querySelector('#cupon')
 
 calcular.addEventListener('click',calculoPrecioConDescuento);
 
+
+const cupones = {
+    'C50' : 50,
+    'C25' : 25,
+    'C10' : 10 
+}
 function calculoPrecioConDescuento () {
-    const precioConDescuento = ((parseInt(precio.value) * (100 - parseInt(descuento.value)))/100);
-    console.log(precioConDescuento);
-    resultado.innerText = precioConDescuento;
+
+    if (descuento.value > 100){
+        resultado.innerText = 'No puede haber un descuento superior al 100%'
+    } else if (!descuento.value && !precio.value && !cupon.value){
+        resultado.innerText = 'No has ingresado ningún valor'
+    }else if (!precio.value ){
+        resultado.innerText = 'No has ingresado el precio'
+    } else if (!descuento.value && !cupon.value){
+        resultado.innerText = 'No has ingresado ningún descuento y/o cupon'
+    }  else if (descuento.value < 100 && descuento.value){
+        const precioConDescuento = ((parseInt(precio.value) * (100 - parseInt(descuento.value)))/100);
+        console.log(precioConDescuento);
+        resultado.innerText = "El " + parseInt(descuento.value) + "% de descuento de $" + parseInt(precio.value) +" es $" + precioConDescuento;
+    } else if (cupones[cupon.value]) {
+        const precioConDescuento = ((parseInt(precio.value) * (100 - parseInt(cupones[cupon.value])))/100);
+        console.log(precioConDescuento);
+        resultado.innerText = "El cupon de descuento " + cupon.value + " tiene un "  + parseInt(cupones[cupon.value]) + "% de descuento, el precio original es de $" + parseInt(precio.value) +" y con el descuento es $" + precioConDescuento;
+    }else {
+        resultado.innerText = 'El cupon no es valido'
+    }
+    
 }
