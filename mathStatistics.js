8182fa6209e9327df6d0ecb20b2a44aade19fb6c
Fausto Mastrodomenico <faustomastro96@gmail.com>
2024-01-22 19:26:00 -0500

    Se agrega el metodo reduce para poder realizar el promedio

--- mathStatistics.js
+++ mathStatistics.js
@@ -1,9 +1,22 @@
 function calcularPromedio (lista){
-    const totalElementos = lista.length;
-    let sumaElementos = 0;
-    for (valor of lista){
-        sumaElementos += valor;
-    }
-    const average = sumaElementos / totalElementos;
+    // const totalElementos = lista.length;
+    // let sumaElementos = 0;
+    // for (valor of lista){
+    //     sumaElementos += valor;
+    // }
+    // const average = sumaElementos / totalElementos;
+    // return average
+
+
+    // function sumarTodosElementos (valorAcumulado, nuevoValor){
+    //     return valorAcumulado + nuevoValor;
+    // }
+
+    // const sumaElementos = lista.reduce(sumarTodosElementos);
+    // const average = sumaElementos / lista.length;
+    // return average
+
+    const sumaElementos = lista.reduce((valorAcumulado, nuevoValor) => valorAcumulado + nuevoValor);
+    const average = sumaElementos / lista.length;
     return average
 }
