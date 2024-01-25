72e49a31f6ea482d08f596d56778c1f53873a7cc
Fausto Mastrodomenico <faustomastro96@gmail.com>
2024-01-24 20:38:01 -0500

    Se creo el objeto de empresas con sus respectivos salarios por año, además de las funciones de mediana de empresas por año y la proyección por empresa

--- analisis.js
+++ analisis.js
@@ -41,3 +41,71 @@ function proyeccionPorPersona (nombrePersona){
 
     return nuevoSalario
 }
+
+const empresas = {};
+
+for (persona of salarios){
+    for (trabajo of persona.trabajos){
+        if (!empresas[trabajo.empresa]){
+            empresas[trabajo.empresa] = {};
+        }
+
+        if (!empresas[trabajo.empresa][trabajo.year]){
+            empresas[trabajo.empresa][trabajo.year] = []
+        }
+
+        empresas[trabajo.empresa][trabajo.year].push(trabajo.salario);
+    }
+}
+
+console.log(empresas);
+
+function medianaEmpresaYear(nombre,year){
+    if (!empresas[nombre]){
+        console.warn('La empresa no existe');
+    } else if( !empresas[nombre][year]){
+        console.warn('La empresa no tuvo registros salariales ese año')
+    } else {
+        return MathStatistics.calcularMediana(empresas[nombre][year])
+    }
+    
+}
+
+function medianaEmpresaYear(nombre,year){
+    if (!empresas[nombre]){
+        console.warn('La empresa no existe');
+    } else if( !empresas[nombre][year]){
+        console.warn('La empresa no tuvo registros salariales ese año')
+    } else {
+        return MathStatistics.calcularMediana(empresas[nombre][year])
+    }
+}
+
+function proyeccionPorEmpresa(nombre){
+    if (!empresas[nombre]){
+        console.warn('La empresa no existe');
+    } else {
+        const empresaYears = Object.keys(empresas[nombre]);
+        const listaMedianaYears = empresaYears.map((year) => {return medianaEmpresaYear(nombre,year)})
+        
+        let porcentajesCrecimiento = [];
+
+        for (let i = 1; i< listaMedianaYears.length; i++){
+            const salarioActual = listaMedianaYears[i];
+            const salarioPasado = listaMedianaYears [i - 1];
+            const crecimiento = salarioActual - salarioPasado;
+            const porcentajeCrecimiento = crecimiento / salarioPasado;
+            console.log(porcentajeCrecimiento);
+            porcentajesCrecimiento.push(porcentajeCrecimiento)
+        }
+        
+        const medianaPorcentajesCrecimiento = MathStatistics.calcularMediana(porcentajesCrecimiento);
+
+        const ultimaMedianaSalarios = listaMedianaYears[listaMedianaYears.length - 1];
+        const nuevaMedianaSalarios = ultimaMedianaSalarios * (1 + medianaPorcentajesCrecimiento);
+    
+        return nuevaMedianaSalarios;
+
+        
+    }
+}
