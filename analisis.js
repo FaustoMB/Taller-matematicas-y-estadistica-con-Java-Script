2c5afa8ce784177163b57909da163357f604cb29
Fausto Mastrodomenico <faustomastro96@gmail.com>
2024-01-24 19:00:01 -0500

    Se agrega la función de proyeccionPorPersona

--- analisis.js
+++ analisis.js
@@ -20,3 +20,24 @@ function medianaPorPersona (nombrePersona){
     
     // MathStatistic.calcularMediana(trabajos.find (sala))
 }
+
+function proyeccionPorPersona (nombrePersona){
+    const trabajos = encontrarPersona(nombrePersona).trabajos;
+
+    let porcentajesCrecimiento = [];
+
+    for (let i = 1; i< trabajos.length; i++){
+        const salarioActual = trabajos[i].salario;
+        const salarioPasado = trabajos [i - 1].salario;
+        const crecimiento = salarioActual - salarioPasado;
+        const porcentajeCrecimiento = crecimiento / salarioPasado;
+        console.log(porcentajeCrecimiento);
+        porcentajesCrecimiento.push(porcentajeCrecimiento)
+    }
+    
+    const medianaPorcentajesCrecimiento = MathStatistics.calcularMediana(porcentajesCrecimiento);
+    const ultimoSalario = trabajos[trabajos.length - 1].salario;
+    const nuevoSalario = ultimoSalario * (1 + medianaPorcentajesCrecimiento);
+
+    return nuevoSalario
+}
